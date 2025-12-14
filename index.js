e476ec0e47afd688872c952bd18991e0f9fc17f3
Cube Nerd <mastercuber55@gmail.com>
2025-12-14 18:59:45 +0530

    added the other files to staging 😭

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-import express from "express"
-import http from "node:http"
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { Server } from "socket.io"
+	import express from "express"
+	import http from "node:http"
+	import { dirname, join } from 'node:path';
+	import { fileURLToPath } from 'node:url';
+	import { Server } from "socket.io"
 
-const app = express()
-const httpServer = http.createServer(app)
-const io = new Server(httpServer)
+	const app = express()
+	const httpServer = http.createServer(app)
+	const io = new Server(httpServer)
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+	const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const messages = []
+	const messages = []
 
-app.use(express.static(join(__dirname, "web")));	
+	app.use(express.static(join(__dirname, "web")));	
 
-io.on("connection", (socket) => {
-	
-	let name = socket.handshake.auth.name || "Anonymous"
+	io.on("connection", (socket) => {
+		
+		let name = socket.handshake.auth.name || "Anonymous"
 
-	socket.emit("history", messages)
+		socket.emit("history", messages)
 
-	io.emit("join", name);
-	messages.push({content: "joined the chat", user: name})
+		io.emit("join", name);
+		messages.push({content: "joined the chat", user: name})
 
-	socket.on("disconnect", () => {
-		io.emit("leave", name)
-		messages.push({content: "left the chat", user: name})
+		socket.on("disconnect", () => {
+			io.emit("leave", name)
+			messages.push({content: "left the chat", user: name})
+		})
+
+		socket.on("message", (msg) => {
+			const data = { content: msg, user: name }
+			messages.push(data)
+			io.emit("message", data)
+		})
 	})
 
-	socket.on("message", (msg) => {
-		const data = { content: msg, user: name }
-		messages.push(data)
-		io.emit("message", data)
+	httpServer.listen(8080, () => {
+		console.log("Running at 8080")
 	})
-})
-
-httpServer.listen(8080, () => {
-	console.log("Running at 8080")
-})
---
+++ web/sw.js
@@ -0,0 +1,22 @@
+var cacheName = 'global-chat-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/script.js'
+];
+
+self.addEventListener('install', ev => {
+	ev.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  )
+  self.skipWaiting()
+})
+
+
+self.addEventListener('fetch', (ev) => 
+	ev.respondWith(
+    	caches.match(ev.request).then(
+    		res => res || fetch(ev.request)
+    	)
+  	)
+);
