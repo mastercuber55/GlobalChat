02e46db3c498cd83544f9fce8094e5b3a1af87be
Cube Nerd <mastercuber55@gmail.com>
2025-12-16 10:33:47 +0530

    system and chat message types seperation

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-	import express from "express"
-	import http from "node:http"
-	import { dirname, join } from 'node:path';
-	import { fileURLToPath } from 'node:url';
-	import { Server } from "socket.io"
+import express from "express"
+import http from "node:http"
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Server } from "socket.io"
 
-	const app = express()
-	const httpServer = http.createServer(app)
-	const io = new Server(httpServer)
+const app = express()
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
 
-	const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-	const messages = []
+const messages = []
 
-	app.use(express.static(join(__dirname, "web")));	
+app.use(express.static(join(__dirname, "web")));	
 
-	io.on("connection", (socket) => {
-		
-		let name = socket.handshake.auth.name || "Anonymous"
+io.on("connection", (socket) => {
+	
+	let name = socket.handshake.auth.name || "Anonymous"
 
+	socket.on("ready", () => {
+		messages.push({	type: "system", content: "joined the chat!", user: name})
 		socket.emit("history", messages)
-
-		io.emit("join", name);
-		messages.push({content: "joined the chat!", user: name})
-
-		socket.on("disconnect", () => {
-			io.emit("leave", name)
-			messages.push({content: "left the chat!", user: name})
-		})
-
-		socket.on("message", (msg) => {
-			const data = { content: msg, user: name }
-			messages.push(data)
-			io.emit("message", data)
-		})
+		socket.broadcast.emit("join", name);
 	})
 
-	httpServer.listen(8080, () => {
-		console.log("Running at 8080")
+	socket.on("disconnect", () => {
+		io.emit("leave", name)
+		messages.push({type: "system", content: "left the chat!", user: name})
 	})
+
+	socket.on("message", (msg) => {
+		const data = { type: "chat", content: msg, user: name }
+		messages.push(data)
+		io.emit("message", data)
+	})
+})
+
+httpServer.listen(8080, () => {
+	console.log("Running at 8080")
+})
--- web/script.js
+++ web/script.js
@@ -18,12 +18,12 @@ createApp({
 		const socket = io({ auth: { name } })
 
 		socket.on("history", msgs => messages.value = msgs)
-
 		socket.on("message", msg => messages.value.push(msg))
 
-		socket.on("join", name => messages.value.push({ user: name, content: "joined the chat!" }))
+		socket.on("join", name => messages.value.push({ type: "system", user: name, content: "joined the chat!" }))
+		socket.on("leave", name => messages.value.push({ type: "system", user: name, content: "left the chat!" }))
 
-		socket.on("leave", name => messages.value.push({ user: name, content: "left the chat!" }))
+		socket.on("connect", () => socket.emit("ready"))
 
 		const send = () => {
 			if(!input.value) return;
