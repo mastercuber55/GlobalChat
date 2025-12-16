1001bae2dca7a6587cc01603a6f745c8be8a0fb9
Cube Nerd <mastercuber55@gmail.com>
2025-12-16 09:59:18 +0530

    using vue.js framework ( client side only )

--- web/script.js
+++ web/script.js
@@ -1,66 +1,36 @@
-let name = localStorage.getItem('name');
-
-while (!name) name = prompt("What is your name??\nNOTE: You won't be able to change it.")
-
-const socket = io({
-	auth: { name }
-})
-
-localStorage.setItem('name', name)
-
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register("sw.js")
 		.then(console.log)
 		.catch(console.error)
 }
-	
-const messages = document.getElementById("messages");
-const input = document.getElementById("messageToSend")
-const submit = document.getElementById("sendMessage")
 
-submit.addEventListener("click", () => {
-	if (!input.value)
-		return
+const { createApp, ref } = Vue
 
-	socket.emit("message", input.value)
-	input.value = ""
-})
+createApp({
+	setup() {
+		const messages = ref([])
+		const input = ref("")
 
-socket.on("history", msgs => {
-	msgs.forEach(msg => appendMessage(msg))
-})
+		let name = localStorage.getItem('name');
+		while (!name) name = prompt("What is your name??\nNOTE: You won't be able to change it.")
+		localStorage.setItem('name', name)
 
-// THIS DOES NOT FIRE WHEN USING socket.emit, OKAY?
-// ITS EMITED ON THE SERVER NOT CLIENT!
-async function appendMessage(msg) {
-	const item = document.createElement("li")
-	const user = document.createElement("b")
-	const content = document.createTextNode(msg.content)
+		const socket = io({ auth: { name } })
 
-	user.textContent = msg.user + ": "
-	item.appendChild(user)
-	item.appendChild(content)
-	messages.appendChild(item)
+		socket.on("history", msgs => messages.value = msgs)
 
-	window.scrollTo(0, document.body.scrollHeight)
-}
+		socket.on("message", msg => messages.value.push(msg))
 
-socket.on("message", msg => appendMessage(msg))
+		socket.on("join", name => messages.value.push({ user: name, content: "joined the chat!" }))
 
-socket.on("join", name => {
-	appendMessage({user: name, content: "joined the chat!"})
-})
+		socket.on("leave", name => messages.value.push({ user: name, content: "left the chat!" }))
 
-socket.on("leave", name => {
-	appendMessage({user: name, content: "left the chat!"})
-})
+		const send = () => {
+			if(!input.value) return;
+			socket.emit("message", input.value)
+			input.value = ""
+		}
 
-input.addEventListener("keydown", async (ev) => {
-	if (ev.key === "Enter") {
-		ev.preventDefault()
-
-		if (!input.value) return
-		socket.emit("message", input.value)
-		input.value = ""
+		return { messages, input, send }
 	}
-})
+}).mount("#app")
